c32d5ca0440632f6cc1e8a7a7a31b64b337178e6
rojajullapalli <rojajules@gmail.com>
2021-09-25 09:18:51 +0530

    ADDED: Calculated Fare Distance And Time With Minimum Fare

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -7,7 +7,17 @@ public class InvoiceServiceTest {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance,time);
-        Assertions.assertEquals(25,fare,0.0);
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assertions.assertEquals(5,fare,0.0);
+    }
+
 }
